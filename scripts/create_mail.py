8dd72406f254f6cb5f41234ec33dfe6a70fd5387
ShotaIuchi <shotaiuchi.develop@gmail.com>
2025-01-11 14:37:53 +0900

    設定ファイルパス修正

--- scripts/create_mail.py
+++ scripts/create_mail.py
@@ -4,31 +4,74 @@ import urllib.parse
 import webbrowser
 import yaml
 
-def load_config():
-    with open(os.path.abspath('config.yml'), 'r', encoding='utf-8') as fp:
+PWD = os.getcwd()
+CWD = os.path.dirname(os.path.abspath(__file__))
+HOME = os.path.expanduser('~')
+
+
+class OpenConfig:
+    def __init__(self, target_filepath, filename):
+        self.filepath = os.path.dirname(os.path.abspath(target_filepath))
+        print(self.filepath)
+        self.filename = filename
+        self.file = None
+
+    def __enter__(self):
+        # target file path
+        filepath = os.path.join(self.filepath, self.filename)
+        if os.path.exists(filepath):
+            self.file = open(filepath, 'r', encoding='utf-8')
+            return self.file
+        # cwd file path
+        filepath = os.path.join(CWD, self.filename)
+        if os.path.exists(filepath):
+            self.file = open(filepath, 'r', encoding='utf-8')
+            return self.file
+        # pwd file path
+        filepath = os.path.join(PWD, self.filename)
+        if os.path.exists(filepath):
+            self.file = open(filepath, 'r', encoding='utf-8')
+            return self.file
+        # home file path
+        filepath = os.path.join(HOME, self.filename)
+        if os.path.exists(filepath):
+            self.file = open(filepath, 'r', encoding='utf-8')
+            return self.file
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.file:
+            self.file.close()
+
+
+def load_config(target_filepath):
+    with OpenConfig(target_filepath, 'config.yml') as fp:
         data = yaml.safe_load(fp)
     return data
 
-def load_env(file):
-    with open(os.path.abspath('env.yml'), 'r', encoding='utf-8') as fp:
+
+def load_env(target_filepath):
+    with OpenConfig(target_filepath, 'env.yml') as fp:
         datas = yaml.safe_load(fp)
     for data in datas:
-        if data['FILE'] in file:
+        if data['FILE'] in target_filepath:
             return data
     return None
+
 
 def load_template(file):
     with open(file, 'r', encoding='utf-8') as fp:
         lines = fp.readlines()
-    
+
     if len(lines) < 1:
         print(f"File {file} is empty")
         sys.exit(10)
-    
+
     title = lines[0]
-    body  = ''.join(lines[2:])
+    body = ''.join(lines[2:])
 
     return title, body
+
 
 def main():
     if len(sys.argv) < 2:
@@ -39,13 +82,13 @@ def main():
     if not os.path.exists(file):
         print(f"File {file} not found")
         sys.exit(2)
-        
+
     env = load_env(file)
     if not env:
         print("Environment variables not found")
         sys.exit(3)
 
-    configs = load_config()
+    configs = load_config(file)
 
     title, body = load_template(file)
     if not title or not body:
@@ -82,6 +125,7 @@ def main():
         mailto += '?' + '&'.join(query_params)
 
     webbrowser.open(mailto)
+
 
 if __name__ == '__main__':
     main()
